83aa806eb226fbcce7ab3c38a6bffbd5748e66f9
LauPss <laurapipio@msn.com>
2021-10-11 22:02:41 +0200

    second attempt

--- src/GetData.js
+++ src/GetData.js
@@ -2,7 +2,7 @@ import GetDate from "./GetDate.js";
 import WeathObjFactory from "./WeathObjFactory.js";
 
 export default async function GetData (location) {
-	const str = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=1c44231a701b8a2ef1a98582516a5a74";
+	const str = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=1c44231a701b8a2ef1a98582516a5a74";
 	GetDate();
 	try {
 		const response = await fetch(str, {mode: "cors"});
